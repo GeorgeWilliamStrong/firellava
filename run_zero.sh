d06e76fff7b9f423deaf4b4c56f97eb116fe74b0
george <george.strong@instill.tech>
2024-07-09 11:22:28 +0100

    feat: add run file for zero

--- run_zero.sh
+++ run_zero.sh
@@ -1,17 +1,16 @@
 accelerate launch \
-    --config_file=accelerate_configs/multi_gpu.yaml \
+    --config_file=accelerate_configs/deepspeed_zero3.yaml \
     --num_processes 8 \
     vsft_llava.py \
         --dataset_name="HuggingFaceH4/llava-instruct-mix-vsft" \
-        --model_name_or_path="llava-hf/llava-1.5-7b-hf" \
+        --model_name_or_path="fireworks-ai/FireLLaVA-13b" \
         --report_to="wandb" \
         --learning_rate=1.4e-5 \
         --per_device_train_batch_size=8 \
         --gradient_accumulation_steps=1 \
-        --output_dir="data/vsft-llava-1.5-7b-hf" \
+        --output_dir="data/vsft-firellava-13b" \
         --logging_steps=5 \
         --num_train_epochs=1 \
-        --push_to_hub \
         --gradient_checkpointing \
         --remove_unused_columns=False \
         --torch_dtype=float16 \
