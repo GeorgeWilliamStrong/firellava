a5ae862219576feb87047f12cd37a4125127d567
George Strong <42313440+GeorgeWilliamStrong@users.noreply.github.com>
2024-08-14 00:33:28 +0100

    Update run_zero.sh

--- run_zero.sh
+++ run_zero.sh
@@ -3,12 +3,12 @@ accelerate launch \
     --num_processes 6 \
     vsft_llava.py \
         --dataset_name="/home/ubitus07/setup2/dataset/hf" \
-        --model_name_or_path="/home/ubitus07/FireLLaVA-13b" \
+        --model_name_or_path="/home/*******/FireLLaVA-13b" \
         --report_to="wandb" \
         --learning_rate=1e-7 \
         --per_device_train_batch_size=8 \
         --gradient_accumulation_steps=1 \
-        --output_dir="/home/ubitus07/experiments/test3/vsft-firellava-13b" \
+        --output_dir="/home/*******/experiments/test3/vsft-firellava-13b" \
         --logging_steps=5 \
         --num_train_epochs=1 \
         --gradient_checkpointing \
